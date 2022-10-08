bdd968d1ae3e5777c9a06aa6d594416c78ee4b30
Sanket <98463682+sanket8495@users.noreply.github.com>
2022-10-08 23:28:59 +0530

    Create binHash.bat

---
+++ binHash.bat
@@ -0,0 +1,16 @@
+incall Synth
+
+rnt base = Synth.setPulse(125**2)
+
+rnt.find /i :: "1137:5360::9281:2422" >Null && goto check (END)
+
+lite:d#000003577
+lite:d#000000567
+lite:d#373889544
+lite:d#0000000084
+lite:d#0000045823
+lite:d#0000446389
+lite:d#0000000002
+lite:d#0000634277
+lite:d#0000000085
+lite:d#0000843721
