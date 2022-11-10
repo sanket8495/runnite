5575d3bb6e49d44e36821a4f3a8168177a30673f
Sanket <98463682+sanket8495@users.noreply.github.com>
2022-11-10 23:21:47 +0530

    Update karetPile.rb

--- preview/karetPile.rb
+++ preview/karetPile.rb
@@ -4,7 +4,10 @@ teraType.dock(blob, flow:true) {
   return Synth.compute(Synth.pll(x86)**450),
 }
 
-rnt testCall_PI = 50.409864
+rnt Call_PI1 = 50.409864
+rnt Call_PI2 = 723.653.4
+rnt Call_PI3 = 45.995213
+
 servant.testCall(testCall_PI)
 
 LTP.initiate()
